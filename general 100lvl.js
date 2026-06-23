585cc86854c6620f9b762465907fdb9076262fb0
Fortune1960 <nelsonfortune0@gmail.com>
2026-06-23 15:36:30 +0100

    100 lvl page

---
+++ general 100lvl.js
@@ -0,0 +1,42 @@
+const tabs = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+
+tab.addEventListener("click", () => {
+
+tabs.forEach(btn =>
+btn.classList.remove("active")
+);
+
+contents.forEach(content =>
+content.classList.remove("active")
+);
+
+tab.classList.add("active");
+
+document
+.getElementById(tab.dataset.tab)
+.classList.add("active");
+
+});
+
+});
+
+
+const deptTitles =
+document.querySelectorAll(".dept-title");
+
+deptTitles.forEach(dept => {
+
+dept.addEventListener("click", () => {
+
+const menu =
+dept.nextElementSibling;
+
+menu.classList.toggle("show");
+
+});
+
+});
+
